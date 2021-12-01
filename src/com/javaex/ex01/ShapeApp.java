1f9ba83ed57a4a59594e1ed3180bde57cdf1a941
이영림 <rm1113@naver.com>
2021-12-01 17:36:23 +0900

    Practice07

---
+++ src/com/javaex/ex01/ShapeApp.java
@@ -0,0 +1,27 @@
+package com.javaex.ex01;
+
+public class ShapeApp {
+
+	public static void main(String[] args) {
+
+		//Shape s = new Shape("빨강");
+		//이거 Shape가 추상클래스라서 그렇습니다아..
+		
+		//Rectangle에러는 추상클래스의 메소드 안써서..
+		
+		
+		Circle c1 = new Circle("녹색", 10);
+		Ractangle r1 = new Ractangle("빨강", 4, 4);
+		Shape sr1 = new Ractangle("빨강", 4, 4);
+		
+		//sr1을 그리는 메소드 호출
+		sr1.draw();
+		//sr1의 가로크기 출력
+		System.out.println(((Ractangle)sr1).getWidth());
+		
+		
+	}
+}
+
+	
+	
