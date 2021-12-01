f50fe43ace107d81e89bc124f45b74bff15a7b8a
이영림 <rm1113@naver.com>
2021-12-01 17:45:19 +0900

    ex02

--- src/com/javaex/ex02/Sparrow.java
+++ src/com/javaex/ex02/Sparrow.java
@@ -1,7 +1,38 @@
 package com.javaex.ex02;
 
 public class Sparrow extends Bird {
+	
+	//필드
+	private String name;
+	
+	
+	//생성자
+	public Sparrow() {
+		
+	}
+	
+	
+	//메소드 g/s
+	public String getName() {
+		return name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
 
+	
+	//메소드 일반
+    public void sing() {
+    	System.out.println("참새("+name+")가 소리내어 웁니다.");
+    }
+
+    public void fly() {
+    	System.out.println("참새("+name+")가 날아 다닙니다.");
+    }
+    
+    public void showName() {
+    	System.out.println("참새의 이름은 "+name+" 입니다.");
+    }
 
 }
